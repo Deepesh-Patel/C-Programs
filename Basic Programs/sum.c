205f5a5888863e671b6053714acaa7efd69e0641
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2021-05-12 18:15:29 +0530

    Addition_of_two_numbers

---
+++ Basic Programs/sum.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+
+    printf("\n********** ENTER TW0 NUMBERS**********\n\n");
+
+    printf("Enter the first number= ");
+    scanf("%d",&a);
+
+    printf("\n");
+
+    printf("Enter the second number= ");
+    scanf("%d",&b);
+
+    printf("\n");
+
+    printf("\n********** ADDITION OF TWO NUMBERS **********\n\n");
+
+    c=a+b;
+    printf("Addition: %d",c);
+
+    return 0;
+}
+
