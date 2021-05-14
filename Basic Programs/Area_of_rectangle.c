636d5ae934370dd02c888c402f64d99816c9cdea
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2021-05-14 19:33:51 +0530

    Area_of_Rectangle

---
+++ Basic Programs/Area_of_rectangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int l, b;
+    
+    printf("\n ********** ENTER TWO NUMBERS **********\n\n");
+    
+    printf("Enter Length and Breadth of rectangle: \n");
+    scanf("%d%d",&l,&b);
+    
+    printf("\n********** AREA **********\n\n");
+    
+    printf("Ares of Rectangle: %d\n",(l*b));
+    
+    return 0;
+}
