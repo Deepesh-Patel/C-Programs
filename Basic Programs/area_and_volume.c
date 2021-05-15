f6515117693c855a85d8b0e2623e073f0a69953f
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2021-05-15 18:40:18 +0530

    Area_and_Volume

---
+++ Basic Programs/area_and_volume.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int l, b;
+    float pi=3.14, r;
+    float h;
+    int s;
+
+    printf("\n******************** ENTER ALL VALUES ********************\n\n");
+
+    printf("Enter Length and Breadth of rectangle:\n");
+    scanf("%d%d",&l,&b);
+
+    printf("Enter Radius of a circle:\n");
+    scanf("%f",&r);
+
+    printf("Enter Side of a square:\n");
+    scanf("%s",&s);
+
+    printf("Enter Height and Radius of cylinder:\n");
+    scanf("%f%f",&h,&r);
+
+    printf("\n******************** AREA ********************\n\n");
+
+    printf("Area of Rectangle: %d\n",(l*b));
+    printf("Area of Circle: %f\n",(pi*r*r));
+    printf("Area of Square: %d\n",(s*s));
+
+    printf("\n******************** VOLUME ********************\n\n");
+
+    printf("Volume of Cylinder: %f\n",(pi*r*r*h));
+
+    return 0;
+}
