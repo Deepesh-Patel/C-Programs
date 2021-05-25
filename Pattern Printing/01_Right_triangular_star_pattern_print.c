1f0781e2de74faca2138db0427d0c8402390d08f
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2021-05-25 14:12:24 +0530

    Star_pattern_01

---
+++ Pattern Printing/01_Right_triangular_star_pattern_print.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int rows;
+    int i, j;
+
+    printf("How many rows do you want? \n");
+    scanf("%d",&rows);
+    printf("\n");
+
+    for(i=0; i<rows; i++)
+    {
+        for(j=0; j<=i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
