089e85b0ae89cfdd94298d17cdac2a48cc5434fe
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2021-06-03 18:20:39 +0530

    Add files via upload

---
+++ Function/05_force_of_attraction_on_body_of_mass.c
@@ -0,0 +1,25 @@
+// Write a function to calculate force of attraction on a body of mass m by earth (g = 9.8 m/s^2).
+
+#include<stdio.h>
+#include<conio.h>
+float force(float mass);
+
+int main()
+{
+    float m;
+
+    printf("Enter the value of mass in kgs:\n");
+    scanf("%f",&m);
+
+    printf("The value of force in Newton is: %.2f\n",force(m));
+
+    getch();
+    return 0;
+}
+
+float force(float mass)
+{
+    float result = mass * 9.8;
+
+    return result;
+}
