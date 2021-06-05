e4d0e778877c5e6bc93adef8d041028eb2cfa394
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2021-06-05 09:54:23 +0530

    Update prime_number.c

--- Looping Statements/prime_number.c
+++ Looping Statements/prime_number.c
@@ -4,10 +4,11 @@
 #include<conio.h> 
 int main()
 {
-    int n, i, m=0, flag=0;    
+    int n, i, flag=0;    
     
-    printf("Enter the number to check prime:");    
-    scanf("%d",&n);    
+    printf("Enter the number to check prime:\n");    
+    scanf("%d",&n);
+    printf("\n");
        
     for(i=2;i<=n/2;i++)    
     {  
