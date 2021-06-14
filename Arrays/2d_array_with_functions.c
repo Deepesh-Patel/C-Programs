04a5288a1c33394032021b2f16f6e705fe7681e5
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2021-06-14 11:00:31 +0530

    Add files via upload

---
+++ Arrays/2d_array_with_functions.c
@@ -0,0 +1,45 @@
+/* Create 2-d array by taking input from the user.
+   Write a display function to print the content of this 2-d array in the screen. */
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    display();
+
+    getch();
+    return 0;
+}
+
+void display(int stud, int sub)
+{
+    stud = 2;
+    sub = 4;
+    int marks[2][4];
+    int i, j;
+
+    for(i=0; i<stud; i++)
+    {
+        for(j=0; j<sub; j++)
+        {
+            printf("Enter the marks of student %d in %d: ", i+1, j+1);
+            scanf("%d", &marks[i][j]);
+        }
+
+        printf("\n");
+    }
+
+    printf("\n");
+    printf("****************************************************\n");
+
+    for(i=0; i<stud; i++)
+    {
+        for(j=0; j<sub; j++)
+        {
+            printf("The marks of student %d in %d is %d\n", i+1, j+1, marks[i][j]);
+        }
+
+        printf("\n");
+    }
+}
