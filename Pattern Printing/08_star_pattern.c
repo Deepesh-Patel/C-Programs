83839fedce9265cb2599659cf5bc1b96177729ee
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2021-07-15 11:01:43 +0530

    Star_Pattern_08

---
+++ Pattern Printing/08_star_pattern.c
@@ -0,0 +1,35 @@
+/*To print the following star pattern
+
+ *   *
+  * *
+   *
+  * *
+ *   *
+          */
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n=5;
+    int i, j;
+
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i==j || i+j==n-1)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
