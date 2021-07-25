1f63cb6bc9b291071c28cbcbcb90b0addfcc7f8d
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2021-07-25 10:36:38 +0530

    Add files via upload

---
+++ File Handling/02_File_Reading.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    FILE *ptr;
+    int num,num2;
+
+    ptr = fopen("Sample.txt","r");
+
+    if(ptr == NULL)
+    {
+        printf("This file dose not exist.\n");
+    }
+    else
+    {
+        fscanf(ptr,"%d",&num);
+        fscanf(ptr,"%d",&num2);
+        fclose(ptr);
+
+        printf("The num is %d\n",num);
+        printf("The num2 is %d\n",num2);
+    }
+
+    getch();
+    return 0;
+}
